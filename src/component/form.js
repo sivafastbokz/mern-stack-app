ee6d7160242e2cad4dbf0965556b45ee3157f294
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-06-03 13:13:37 +0530

    updated

--- src/component/form.js
+++ src/component/form.js
@@ -5,8 +5,29 @@ class Form extends React.Component{
     render(){
         return(
             <React.Fragment>
-             ENTER YOUR age <input name="name"/>
+            <div className="form">
+             ENTER YOUR NAME: <input name="name"/>
              <button>ADD</button>
+             </div>
+             <table>
+                <tr>
+                    <th>SNO</th>
+                    <th>NAME</th>
+                    <th>ACTION</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td></td>
+                    <td><button className="button1">DELETE</button></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                    <td><button className="button1">DELETE</button></td>
+                </tr>
+             </table>
+             
+           
             </React.Fragment>
         )
     }
