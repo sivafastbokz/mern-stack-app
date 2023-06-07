4ca15a3ad626a34450974503bf71040b56e3cc0e
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-06-07 18:11:01 +0530

    delete completed

--- src/component/form.js
+++ src/component/form.js
@@ -1,10 +1,13 @@
-import React, { useState,useEffect } from "react";
+import React, { useState,useEffect,  } from "react";
 import '/home/siva/Desktop/task1/myapp/src/component/form.css'
 import axios from "axios";
+
+const numbers = [1,2,3,4,5]
 
 function Form(){
     const[userName,setUserName]=useState("");
     const[Users,setAllUsers]=useState([]);
+     const[users,setUsers]=useState([]);
 
     const addnew =()=>{
         axios.post("http://localhost:5000/adduser",{
@@ -20,6 +23,26 @@ function Form(){
       useEffect(() => {
         userlist();
       }, []);
+
+      const deleteuser = (id) => {
+        axios.delete(`http://localhost:5000/deleteuser/${id}`)
+          .then(response => {
+            setUsers(users.filter(user => user.id !== id));
+            
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      };
+  
+
+
+ let maps= numbers.map((i)=>{
+    if( i<3){
+        return i
+        }
+ })
     return(
         <React.Fragment>
             <div className="form">
@@ -33,13 +56,26 @@ function Form(){
                  <th>NAME</th>
                 <th>ACTION</th>
                 </tr>
-                {Users.map(user => (
+                {Users.map(user => ( 
                 <tr key={user.id}>
                 <td>{user.userName}</td>
-                <td><button className="button1">DELETE</button></td>
+                <td><button className="button1" onClick={()=>{
+                    deleteuser(user._id)
+                    
+                }}>DELETE</button></td>
               </tr>
             ))}
             </table>
+
+
+            <div>
+                <>Top 3 points</>
+                
+                        <p>{maps}</p>
+
+                    
+                
+            </div>
         </React.Fragment>
     )
 }
