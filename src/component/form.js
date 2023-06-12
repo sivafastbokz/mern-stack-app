f61dac915bb10569988f2f7a0e269c627ff82fe0
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-06-12 21:59:23 +0530

    compeleted

--- src/component/form.js
+++ src/component/form.js
@@ -5,16 +5,19 @@ function Form(){
         const[userName,setUserName]=useState("");
         const[Users,setAllUsers]=useState([]);
         const[users,setUsers]=useState([]);
+        // const[person,updateperson]=useState('');
+        
 
         const addnew = async()=>{
         window.alert('your name has been added')
-        await axios.post("http://localhost:5000/adduser",{
+        await axios.post("http://localhost:9000/adduser",{
             userName:userName
+            
         });
         handlereload();
     };
          const userlist = () => {
-         axios.get("http://localhost:5000/getuserlist").then((res) => {
+         axios.get("http://localhost:9000/getuserlist").then((res) => {
          setAllUsers(res.data)}
          );
       };
@@ -24,7 +27,7 @@ function Form(){
 
         const deleteuser = (id) => {
         window.confirm('are you sure?')
-        axios.delete(`http://localhost:5000/deleteuser/${id}`)
+        axios.delete(`http://localhost:9000/deleteuser/${id}`)
           .then(response => {
             setUsers(users.filter(user => user.id !== id));
             console.log(response.data);
@@ -35,16 +38,15 @@ function Form(){
           handlereload();
       };
       
-        const updateuser = (id) =>{
-        const newName = prompt('Enter the new name')
-        axios.put(`http://localhost:5000/update/${id}`,{
-          userName:newName
+        const updateuser = (id,userName) =>{
+          console.log('siva',id,userName)
+        const newName = setUserName(userName)
+        axios.put(`http://localhost:9000/update/${id}`,{
+           userName:newName
         })
-       handlereload();
-          // const up = alert(id)
+        //  handlereload();
+          
       }
-
-
         const handlereload =()=>{
          window.location.reload();
       }
@@ -52,9 +54,7 @@ function Form(){
     return(
         <React.Fragment>
             <div className="form">
-             CREATE CONTACTS: <input type='text'value={userName} onChange={(event)=>{
-                setUserName(event.target.value)
-             }}/>
+             CREATE CONTACTS:<input type='text' value={userName} onChange={(event)=>setUserName(event.target.value)}></input>
              <button onClick={addnew}>ADD NEW</button>
              </div>
              <table>
@@ -67,7 +67,7 @@ function Form(){
                 <tr key={user.id}>
                 <td>{user.userName}</td>
                 <td><button className="button1" onClick={()=>{deleteuser(user._id)}}>DELETE</button></td>
-                    <td><button className="button2" onClick={()=>{updateuser(user._id)}}>UPDATE</button></td>
+                    <td><button className="button2" onClick={()=>{updateuser(user._id,user.userName)}}  >UPDATE</button></td>
               </tr>
             ))}
             </table>
